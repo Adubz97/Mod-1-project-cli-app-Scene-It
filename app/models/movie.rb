ead67e414026b39b9deeb397ea560e8b999e15c3
chsohn15 <chsohn15@gmail.com>
2020-07-29 11:28:02 -0400

    Added method to Movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,10 @@ class Movie < ActiveRecord::Base
     has_many :movie_locations
     has_many :locations, through: :movie_locations
 
-
+    def self.find_movie(user_input_movie)
+        self.all.select do |movie|
+            movie.name.downcase == user_input_movie.downcase
+        end
+    end
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,8 @@
 require_relative '../config/environment'
 
+puts "Welcome to Scene It!"
+puts "This is an app designed to find movie and TV show film locations from all around the world!  
+"
 
 
-puts "HELLO WORLD"
 binding.pry
