e7fa15b5cb545938c8029b04d9d987b2b16a1e2f
chsohn15 <chsohn15@gmail.com>
2020-07-29 11:41:01 -0400

    Added more class methods

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,5 +3,17 @@ class Location < ActiveRecord::Base
     has_many :movie_locations
     has_many :movies, through: :movie_locations
 
+    def self.find_location_by_name(user_input_location_name)
+        self.all.select do |location|
+            location.name.downcase == user_input_location_name.downcase
+        end
+    end
+
+    def self.find_location_by_city(user_input_city, user_input_country)
+        self.all.select do |location|
+            location.city.downcase == user_input_city.downcase && 
+            location.country.downcase == user_input_country.downcase
+        end
+    end
 
 end
--- app/models/movie_location.rb
+++ app/models/movie_location.rb
@@ -5,4 +5,11 @@ belongs_to :movie
 belongs_to :location
 has_many :reviews
 
+    def self.find_movie_locations_by_movie(movie_object)
+        self.all.select do |movielocation|
+            movielocation.movie_id == movie_object.id
+        end
+    end
+
+    
 end
